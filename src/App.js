4b69b6e50e01676fed9b256c6222592cd9d274c6
Lexe <ngocle.ng1001@gmail.com>
2024-08-16 21:41:33 +0700

    feat: setup router for auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { useRoutes } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import Auth from "./features/auth";
+import EmployeePoll from "./features/employee-poll";
+import LoginPage from "./features/auth/pages/Login";
+import SignUpPage from "./features/auth/pages/SignUp";
 
 const App = () => {
-  return <div>App</div>;
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <EmployeePoll />,
+      children: []
+    },
+    {
+      path: "auth",
+      element: <Auth />,
+      children: [
+        {
+          path: "",
+          element: <LoginPage />
+        },
+        {
+          path: "signup",
+          element: <SignUpPage />
+        }
+      ]
+    },
+    {
+      path: "*",
+      element: <NotFound />
+    }
+  ]);
+
+  return routes;
 };
 
 export default App;
---
+++ src/features/employee-poll/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EmployeePoll = () => {
+  return <div>EmployeePoll</div>;
+};
+
+export default EmployeePoll;
