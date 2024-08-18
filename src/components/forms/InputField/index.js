639eabee18ab9ed1ae07a4482f7bdfcce1c08539
Lexe <ngocle.ng1001@gmail.com>
2024-08-18 22:24:52 +0700

    feat: enhance input field component

--- src/components/forms/InputField/index.js
+++ src/components/forms/InputField/index.js
@@ -3,30 +3,41 @@ import PropTypes from "prop-types";
 import classNames from "classnames/bind";
 
 import styles from "./InputField.module.scss";
+import FormField from "../FormField";
 
 const cx = classNames.bind(styles);
 
 /**
  *
- * @param {{id: string, name: string, placeholder: string, type: 'email'|'text'|'password'}} props
+ * @param {{id: string, name: string, placeholder: string, type: 'email'|'text'|'password', label: string, required: boolean}} props
  */
 
-const InputField = ({ id, name, placeholder, type }) => {
+const InputField = ({
+  id,
+  name,
+  placeholder,
+  type = "text",
+  label,
+  required
+}) => {
   return (
-    <input
-      className={cx("input-field")}
-      id={id}
-      name={name}
-      type={type}
-      placeholder={placeholder}
-    />
+    <FormField id={id} label={label} required={required}>
+      <input
+        className={cx("input-field")}
+        id={id}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+      />
+    </FormField>
   );
 };
 
 InputField.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string.isRequired
+  placeholder: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["email", "text", "password"])
 };
 
 export default InputField;
