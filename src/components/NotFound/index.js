546534708a2f7727b95ad7f4decae91be170cf31
Lexe <ngocle.ng1001@gmail.com>
2024-08-22 00:13:11 +0700

    feat: implement not found page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import classNames from "classnames/bind";
+import { useNavigate } from "react-router-dom";
+
+import Button from "../forms/Button";
+import Container from "../Container";
+
+import styles from "./NotFound.module.scss";
+
+const cx = classNames.bind(styles);
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const handleBackToHomeClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <Container className={cx("not-found")}>
+      <h1>Opp! Not found!</h1>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleBackToHomeClick}
+      >
+        Back to home
+      </Button>
+    </Container>
+  );
 };
 
 export default NotFound;
